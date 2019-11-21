13b883c8f0ae74a300e93029832ee629b3a7475c
jeffeggink <jpeggink@gmail.com>
2019-11-21 15:22:35 -0800

    idfk

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+const num1 = 9
+
+const num2 = 1
+
+let cars = ["Aston", "Lexus", "Toyota", "porsche"];
+console.table(cars + " " + "whaat");
+
+let zombies = ["none", "twothee", ];
+console.table(zombies + " " + 'dafuq')
+// console.table(names + zombies 'wtf' + 'lol')
+
+function hello(name) {
+    console.log('what' + name)
+  }
+hello('jeff');
+
+
+if (num1 > num2) {
+    console.log('yay');
+} else {
+   console.log('boo')
+};
